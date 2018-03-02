50b1c81329b35d62c6e3f855cbf8a68a681ef4c7
azzhan <aceziyizhan@hotmail.com>
2018-03-02 15:00:14 -0500

    resolved x-frame issue

---
+++ background.js
@@ -0,0 +1,16 @@
+// Works around X-frame error
+
+var HEADERS_TO_STRIP_LOWERCASE = [
+    'content-security-policy',
+    'x-frame-options',
+];
+chrome.webRequest.onHeadersReceived.addListener(
+    function(details) {
+        return {
+            responseHeaders: details.responseHeaders.filter(function(header) {
+                return HEADERS_TO_STRIP_LOWERCASE.indexOf(header.name.toLowerCase()) < 0;
+            })
+        };
+    }, {
+        urls: ["<all_urls>"]
+    }, ["blocking", "responseHeaders"]);
